609780687c506b1a6838357ecfe194156e722fc7
ViorelBusuioc <129962725+ViorelBusuioc@users.noreply.github.com>
2023-08-03 13:50:44 +0300

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,27 @@
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        if (radius < 0) {
+            radius = 0;
+        } else {
+            this.radius = radius;
+        }
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+}
